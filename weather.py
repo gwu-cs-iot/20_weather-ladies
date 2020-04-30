5b741b93e1c7ac266cfaf7e8e5d15d7b21d58e07
lrshpak <lrshpak@gwu.edu>
2020-04-30 14:07:34 -0400

    can get data from api

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import gpiozero
 from time import sleep
-
+import json
+import requests
 
 #Set up pins
 Forward = gpiozero.OutputDevice(23)
@@ -11,17 +12,32 @@ East = gpiozero.LED(3)
 South = gpiozero.LED(17)
 West = gpiozero.LED(27)
 try:
+   
     while True:
         Forward.on()
-
-        speedFlag = float(input("set input speed 0-240: "))
-        print("Please input a direction, ex: N or SW, exit to end")
-        direction = raw_input()
-
-        if speedFlag > 240 or speedFlag < 0:
-            speedFlag = float(input("set input speed 0-240: "))
+        ##set up API Call
+        print("Please enter a city name: ")
+        city = raw_input()
+        print("Please input the corresponding state: ")
+        state = raw_input()
+        URL="https://api.openweathermap.org/data/2.5/weather"
+        #make call with url
+        PARAMS = {'q':[city,state],'appid':'745774c81d84372e1972cab0a2bdc2c2'}
+        response = requests.get(url = URL, params = PARAMS)
+        data = response.json()
+        rawSpeed= data["wind"]["speed"]
+        print("raw speed = {} ".format(rawSpeed))
+        rawDeg = data["wind"]["deg"]
+        print("raw deg = {}".format(rawDeg))
+        direction = 'N'
+        #convert speed from m/s to mph
+        convertedSpeed = rawSpeed * 2.237
+        #highest ever wind speed = 231 used 240 as a buffer
+        speedFlag = convertedSpeed / 240
         
-        SpeedPWM.value = speedFlag/240
+        #set speed for motor
+        SpeedPWM.value = speedFlag
+        
         if direction == 'N':
             North.on()
             sleep(5)
