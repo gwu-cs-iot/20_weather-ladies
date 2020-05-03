45a5dd4fd8da29f43a68d232372505e08530a1ed
Tuhina Dasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2020-05-03 10:54:55 -0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -18,14 +18,16 @@ try:
     while goAgain == 'Y':
         Forward.on()
         try:
-            #get city and state data from user
+            #get city, state, country code data from user
             print("Please enter a city name: ")
             city = raw_input()
             print("Please input the corresponding state: ")
             state = raw_input()
+            print("Please input the corresponding country code: ")
+            country = raw_input()
         
             URL="https://api.openweathermap.org/data/2.5/weather"
-            PARAMS = {'q':[city,state],'appid':'745774c81d84372e1972cab0a2bdc2c2'}
+            PARAMS = {'q':[city,state,country],'appid':'745774c81d84372e1972cab0a2bdc2c2'}
             response = requests.get(url = URL, params = PARAMS)
             data = response.json()
             rawSpeed= data["wind"]["speed"]
@@ -40,55 +42,55 @@ try:
             SpeedPWM.value = speedFlag
 
         except:
-            print("No data for {}, {}".format(city,state))
+            print("No data for {}, {}, {}".format(city,state,country))
             exit()
 
         #each direction gets 45 degrees on the compass
         if direction > 337.5 or direction <= 22.5:
             North.on()
-            print("In {}, {} the wind is blowing North at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing North at a speed of {} mph.\n".format(city,state, country,convertedSpeed))
             sleep(5)
             North.off()
         if direction > 67.5 and direction <=112.5:
             East.on()
-            print("In {}, {} the wind is blowing East at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing East at a speed of {} mph.\n".format(city,state,country,convertedSpeed))
             sleep(5)
             East.off()
         if direction > 167.5 and direction <= 202.5:
             South.on()
-            print("In {}, {} the wind is blowing South at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing South at a speed of {} mph.\n".format(city,state,country,convertedSpeed))
             sleep(5)
             South.off()
         if direction > 247.5 and direction <= 292.5:
             West.on()
-            print("In {}, {} the wind is blowing West at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing West at a speed of {} mph.\n".format(city,state,country, convertedSpeed))
             sleep(5)
             West.off()
         if direction > 22.5 and direction <= 67.5:
             North.on()
             East.on()
-            print("In {}, {} the wind is blowing Northeast at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing Northeast at a speed of {} mph.\n".format(city,state,country,convertedSpeed))
             sleep(5)
             North.off()
             East.off()
         if direction > 112.5 and direction <= 167.5:
             South.on()
             East.on()
-            print("In {}, {} the wind is blowing Southeast at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing Southeast at a speed of {} mph.\n".format(city,state,country,convertedSpeed))
             sleep(5)
             South.off()
             East.off()
         if direction > 202.5 and direction <= 247.5:
             South.on()
             West.on()
-            print("In {}, {} the wind is blowing Southwest at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing Southwest at a speed of {} mph.\n".format(city,state,country,convertedSpeed))
             sleep(5)
             South.off()
             West.off()
         if direction > 292.5 and direction <= 337.5:
             North.on()
             West.on()
-            print("In {}, {} the wind is blowing Northwest at a speed of {} mph.\n".format(city,state,convertedSpeed))
+            print("In {}, {}, {} the wind is blowing Northwest at a speed of {} mph.\n".format(city,state,country,convertedSpeed))
             sleep(5)
             North.off()
             West.off()
